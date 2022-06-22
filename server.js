62cddfa815af93272fa86febf0169d49d4e7c1a5
sibongilek <46591239+sibongilek@users.noreply.github.com>
2022-06-22 09:10:57 +0200

    project commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('.dist/mdb-angular-ui-kit-free/multiCorousal'));
+app.use(express.static('.dist/multiCorousal/mdb-angular-ui-kit-free'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/mdb-angular-ui-kit-free/multiCorousal/'}),
+    res.sendFile('index.html', {root: 'dist/multiCorousal/mdb-angular-ui-kit-free/'}),
 );
 
 // Start the app by listening on the default Heroku port
